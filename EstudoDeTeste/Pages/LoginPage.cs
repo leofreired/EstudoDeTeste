5f58ef458437344a9415573054989fa1ae3238a2
leofr <leofr@DESKTOP-OP18IAO>
2025-07-13 19:59:25 -0300

    Adicionar arquivos de projeto.

---
+++ EstudoDeTeste/Pages/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+
+namespace EstudoDeTeste.Pages
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver driver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+
+        public void PreencherUsuario(string usuario)
+        {
+            driver.FindElement(By.Id("username")).SendKeys(usuario);
+        }
+
+        public void PreencherSenha(string senha)
+        {
+            driver.FindElement(By.Id("password")).SendKeys(senha);
+        }
+
+        public void ClicarLogin()
+        {
+            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+        }
+
+        public bool MensagemDeSucessoVisivel()
+        {
+            return driver.PageSource.Contains("You logged into a secure area!");
+        }
+
+        public bool MensagemDeErroVisivel()
+        {
+            return driver.PageSource.Contains("Your password is invalid!")
+                || driver.PageSource.Contains("Your username is invalid!");
+        }
+
+        public void RealizarLogin(string usuario, string senha)
+        {
+            PreencherUsuario(usuario);
+            PreencherSenha(senha);
+            ClicarLogin();
+        }
+    }
+}
---
+++ EstudoDeTeste/Tests/LoginTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using EstudoDeTeste.Pages;
+using System;
+
+namespace EstudoDeTeste.Tests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private IWebDriver driver;
+        private LoginPage loginPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://practice.expandtesting.com/login");
+            loginPage = new LoginPage(driver);
+        }
+
+        [Test]
+        public void LoginComCredenciaisValidas_DeveExibirMensagemDeSucesso()
+        {
+            loginPage.RealizarLogin("practice", "SuperSecretPassword!");
+            Assert.IsTrue(loginPage.MensagemDeSucessoVisivel());
+        }
+
+        [Test]
+        public void LoginComCredenciaisInvalidas_DeveExibirMensagemDeErro()
+        {
+            loginPage.RealizarLogin("practice", "senhaErrada");
+            Assert.IsTrue(loginPage.MensagemDeErroVisivel());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
